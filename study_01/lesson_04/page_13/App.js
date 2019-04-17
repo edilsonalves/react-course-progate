f9ea2d2b69a5d81280598ef08363bb19ab2d4848
Edilson Alves <email@edilsonalves.com>
2019-04-17 18:35:14 -0300

    Lesson 04 completed (Study 01)

---
+++ study_01/lesson_04/page_13/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button>+</button>
+      </div>
+    );
+  }
+}
+
+export default App;
